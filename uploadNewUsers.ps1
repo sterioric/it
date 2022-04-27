3c69adae2738d016d39ca937a7a72c1b83cd2c22
sterioric <68709620+sterioric@users.noreply.github.com>
2022-04-27 13:28:42 -0400

    Update uploadNewUsers.ps1

--- uploadNewUsers.ps1
+++ uploadNewUsers.ps1
@@ -7,7 +7,7 @@ if (Test-Path .\GrandSlam.csv == true) { exit }
 
 foreach ($User in $ADUsers)
 {
- $username = $User.SamAccountName
+ $Username = $User.SamAccountName
  $Password = $User.password
  $Path1 = $User.path
  $GivenName = $User.GivenName
@@ -15,4 +15,32 @@ foreach ($User in $ADUsers)
  $Initials = $User.Initials
  $Name = $User.Name
  $DisplayName = $User.DisplayName
+ $UserPrincipalName = $User.UserPrincipalName
+ $Department = $User.Department
+ $Email = $User.EmailAddress
+ $Title = $User.Title
+ $groups = $User.groups -split ";"
  
+ if(Get-ADUser -F {SamAccountName -eq $Username})
+ {
+ Write-Host Warning
+ }
+ else
+{
+        New-ADUser `
+            -SamAccountName $Username `
+            -UserPrincipalName "$Username@gmail.com" `
+            -Name "$Firstname $Lastname" `
+            -GivenName $Firstname `
+            -Surname $Lastname `
+            -Enabled $True `
+            -DisplayName "$Firstname $Lastname" `
+            -EmailAddress "$Username@gmail.com" `
+            -AccountPassword (convertto-securestring $Password -AsPlainText -Force) `
+            -PasswordNeverExpires $True
+            -Department = $User.Department
+            
+            foreach($group in $groups){Add-ADGroupMember $group -Members $Username}
+            
+            Write-Ouput "$Username added to AD" -ForegroundColor Cyan
+ }
